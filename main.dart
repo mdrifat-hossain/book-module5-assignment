71c74d095ecb99d4d315671a3d29060b2f28a71a
mdrifat-hossain <rifathossain4788@gmail.com>
2024-09-26 21:04:59 +0600

    update code

--- main.dart
+++ main.dart
@@ -1,19 +1,19 @@
 import 'book.dart';
 
 void main() {
-  // Creating 3 Book objects and storing them in a List
+
   List<Book> books = [
     Book('Amar Cotobela', 'Rifat Hossain', 2010),
     Book('Mamar Bari', 'Asif Uddin', 1990),
     Book('Vuter Golpo', 'Sefat Khan', 1950)
   ];
 
-  // Simulating reading pages
+
   books[0].read(50);
   books[1].read(100);
   books[2].read(75);
 
-  // Looping through each book and printing its details
+
   for (var i = 0; i < Book.totalBooks; i++) {
     print('Title: ${books[i].getTitle()}');
     print('Author: ${books[i].getAuthor()}');
@@ -23,6 +23,6 @@ void main() {
     print('\n');
   }
 
-  // Print total number of books created
+
   print('Total Books Created: ${Book.totalBooks}\n');
 }
